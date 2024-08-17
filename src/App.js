8de92c1c353e38e6945bba9db61d59115d6f6c5a
G P GUHAA PRIYAN <125464557+GUH1N@users.noreply.github.com>
2024-08-17 12:07:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App(){
+    const[users,setUsers] = useState([]);
+    const[loading,setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => {
+            setUsers(data);
+            setLoading(false);
+    })
+    .catch(error => {
+        console.log("not loading");
+        setLoading(false);
+    });
+},[]);
+if(loading){
+    return <p>loading</p>;
 }
-
-export default App;
+return(
+    <div>
+        <h1>List of users in API</h1>
+        <ul>
+            {users.map(user => (
+                <li key={user.id}>{user.name}</li>
+            ))}
+        </ul>
+    </div>
+);
+}
